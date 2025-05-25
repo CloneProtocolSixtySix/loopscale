7d714c4a30b82174e63b6aac20fcf1e0f6bb7349
cloneprotocolsixtysix <Yannick.bruderlein@icloud.com>
2025-05-24 20:03:19 -0400

    Remove local font dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,6 @@
 import React from 'react';
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
-
-const altform = localFont({
-  src: [
-    {
-      path: './fonts/altform-light.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: './fonts/altform-regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/altform-semibold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: './fonts/altform-lightitalic.woff2',
-      weight: '300',
-      style: 'italic',
-    },
-    {
-      path: './fonts/altform-regularitalic.woff2',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: './fonts/altform-semibolditalic.woff2',
-      weight: '600',
-      style: 'italic',
-    }
-  ],
-  variable: '--font-altform',
-});
 
 export const metadata: Metadata = {
   title: "Loopscale | We're refining frontier AI",
@@ -49,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${altform.variable}`}>
-      <body className="font-altform font-normal">{children}</body>
+    <html lang="en">
+      <body className="font-sans">{children}</body>
     </html>
   );
 } 
