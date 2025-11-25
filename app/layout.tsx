3358f8a915e22ad7a9520e66f33ffa5d3f6b9625
cloneprotocolsixtysix <Yannick.bruderlein@icloud.com>
2025-11-25 13:06:48 -0500

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Metadata } from 'next';
 import './globals.css';
 export const metadata: Metadata = {
-  title: "Symbolon AI",
+  title: "Augnition",
 };
 
 export default function RootLayout({
