b8f40bc69f079faeab5224f3434a7742456dccda
cloneprotocolsixtysix <Yannick.bruderlein@icloud.com>
2025-12-21 11:18:08 -0500

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { Metadata } from 'next';
 import './globals.css';
 export const metadata: Metadata = {
-  title: "Foretoken",
+  title: "General Intellection",
 };
 
 export default function RootLayout({
